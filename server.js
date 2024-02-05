f3477e5873f369100785dc8c6cca3bbf638980ff
Makarem12 <makaremabososain@gmail.com>
2024-02-05 12:25:22 +0300

    updated

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const moviedata = require('./data.json')
 app.get('/', movieHandler)
+app.get('/favorite', favoriteHandler) 
+
+//functions
 function movieHandler(req,res){
-    const result = []
         let movie = new consMovie(moviedata.title, moviedata.poster_path, moviedata.overview)
-        result.push(movie)
-    res.json(result)
+    res.json(movie)
 }
 function consMovie(title, poster_path, overview){
     this.title = title,
     this.poster_path = poster_path
     this.overview = overview
 }
-app.get('/favorite', favoriteHandler) 
-
 function favoriteHandler(req,res){
     res.send("welcome to favorite page")
     
 }
 app.use((req, res, next) => {
-    res.status(404).json({
-        status: 404,
-        responseText: "Page not found"
-    });
+    res.status(404).send("page not found")
 });
+//internal server error
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({
-        status: 500,
-        responseText: "Sorry, something went wrong"
+    res.status(500).send("Sorry, something went wrong")
     });
-});
 
 app.listen(port, () => {
     console.log(`my app is running and  listening on port ${port}`)
